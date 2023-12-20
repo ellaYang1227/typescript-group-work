af0ae8dc63839d3f7ee09f1cbbf0e2dc82b79a9d
yanenn <sh5ojkmt@gmail.com>
2023-12-20 21:47:13 +0800

    chore: vee-validate 4

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue'
 
 import '@/styles/index.scss'
+import '@/plugins/index.ts'
 import 'bootstrap'
-createApp(App).mount('#app')
+// 引入 VeeValidate 元件跟功能
+import { veeComponentPlugin } from './plugins/vee-component'
+
+import App from './App.vue'
+
+const app = createApp(App);
+
+// 掛載 Global 的 VeeValidate 元件
+// app.component('VField', Field);
+// app.component('VForm', Form);
+// app.component('ErrorMessage', ErrorMessage);
+
+app.use(veeComponentPlugin).mount('#app')
---
+++ src/plugins/vee-component.ts
@@ -0,0 +1,12 @@
+
+import {
+  Field, Form, ErrorMessage
+} from 'vee-validate';
+import type { App } from 'vue'
+export const veeComponentPlugin = {
+  install: (app:App) => {
+    app.component('VField', Field);
+    app.component('VForm', Form);
+    app.component('ErrorMessage', ErrorMessage);
+  }
+};
