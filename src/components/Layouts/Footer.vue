a97877dcce3a88827399a62a449735477471b2dd
yanenn <sh5ojkmt@gmail.com>
2023-12-22 17:08:26 +0800

    feat: footer rwd

--- src/components/Layouts/Footer.vue
+++ src/components/Layouts/Footer.vue
@@ -6,7 +6,7 @@ import BaseButton from '@/components/BaseButton.vue';
 <template>
   <footer class="bg-neutral text-white footer">
     <div class="container-xxl footer_inner">
-      <div class="fotter_top d-flex justify-content-between">
+      <div class="fotter_top d-md-flex justify-content-between">
         <div class="footer_leftSide">
           <img src="@/assets/logoWhite.svg" alt="">
           <div class="mt-5">
@@ -19,31 +19,31 @@ import BaseButton from '@/components/BaseButton.vue';
           </div>
         </div>
         <div class="footer_rightSide">
-          <div class="d-flex">
+          <div class="d-md-flex">
             <div class="leftItem">
               <h4 class="h6">TEL</h4>
               <span class="text-neutral-40">+886-7-1234567</span>
             </div>
-            <div class="">
+            <div class="mt-md-0 mt-3">
               <h4 class="h6">MAIL</h4>
               <span class="text-neutral-40">elh@hexschool.com</span>
             </div>
           </div>
-          <div class="d-flex">
-            <div class="leftItem mt-5">
+          <div class="d-md-flex">
+            <div class="leftItem mt-md-5 mt-3">
               <h4 class="h6">FAX</h4>
               <span class="text-neutral-40">+886-7-1234567</span>
             </div>
-            <div class=" mt-5">
+            <div class="mt-md-5 mt-3">
               <h4 class="h6">WEB</h4>
               <span class="text-neutral-40">www.elhhexschool.com.tw</span>
             </div>
           </div>
         </div>
       </div>
-      <div class="footer_copy d-flex justify-content-between">
-        <p>806023 台灣高雄市新興區六角路123號</p>
-        <p>© 享樂酒店 2023 All Rights Reserved.</p>
+      <div class="footer_copy d-md-flex justify-content-between">
+        <p class="mb-md-0 mb-3">806023 台灣高雄市新興區六角路123號</p>
+        <p class="mb-0">© 享樂酒店 2023 All Rights Reserved.</p>
       </div>
     </div>
   </footer>
@@ -71,6 +71,12 @@ import BaseButton from '@/components/BaseButton.vue';
     margin-top: 2px;
     .leftItem {
       margin-right: 80px;
+    }
+    @include media-breakpoint-down(md) {
+      margin-top: 40px;
+      .leftItem {
+        margin-right: 0;
+      }
     }
   }
   &_copy {
