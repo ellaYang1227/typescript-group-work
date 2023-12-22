31dfbfb0ba4ec44388afd5f0b74e52a72749047e
yanenn <sh5ojkmt@gmail.com>
2023-12-23 00:18:34 +0800

    feat: loading

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import BaseLoading from "@/components/BaseLoading.vue";
 </script>
 
 <template>
+  <BaseLoading />
   <RouterView></RouterView>
 </template>
