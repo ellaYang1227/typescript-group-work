d009e5eae0c8527d6b2a5a70ee144aa3262a0b3e
yanenn <sh5ojkmt@gmail.com>
2023-12-23 21:36:56 +0800

    fix: import 路徑修改

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import { User } from "@/models/api";
 import axios from "axios";
-import { IUserInfo } from "@/interface/auth";
+import { IUserInfo } from "@/Interface/auth";
 import { ref } from "vue";
 export const useAuthStore = defineStore("useAuthStore", () => {
   const currentUser = ref<IUserInfo | null>(null);
