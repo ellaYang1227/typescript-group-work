d4f126e168feab0205b9be4eb20df7550d7f9e64
yanenn <sh5ojkmt@gmail.com>
2023-12-24 18:50:15 +0800

    fix: 更改 loading 樣式

--- src/stores/loading.ts
+++ src/stores/loading.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useLoadingStore = defineStore("loading", () => {
-  const isShowLoading = ref(false);
+  const isShowLoading = ref(true);
 
   const showLoading = () => {
     isShowLoading.value = true;
