2298638eecef2f7a1ed774bd27e9ad202e5565d6
yanenn <sh5ojkmt@gmail.com>
2023-12-24 19:56:18 +0800

    fix: loading 狀態改回

--- src/stores/loading.ts
+++ src/stores/loading.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useLoadingStore = defineStore("loading", () => {
-  const isShowLoading = ref(true);
+  const isShowLoading = ref(false);
 
   const showLoading = () => {
     isShowLoading.value = true;
