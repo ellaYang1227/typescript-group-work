584c37ebcdefb765494e36c35bf3106c005e0650
yanenn <sh5ojkmt@gmail.com>
2023-12-25 21:26:42 +0800

    feat: api 改寫

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,8 +1,15 @@
 import request from "@/plugins/axios";
 
-export function apiGetUser() {
-  return request({
-    url: "/api/v1/user/",
-    method: "get",
-  });
-}
+export const apiGetUser = () => {
+  return request.get(`/api/v1/user`);
+};
+
+export const apiLogin = (data: object) => {
+  return request.post(
+    `/api/v1/user/login`,
+    {
+      ...data,
+    },
+    { headers: { noShowLoading: true } },
+  );
+};
--- src/models/home.ts
+++ src/models/home.ts
@@ -1,8 +1,4 @@
 import request from "@/plugins/axios";
-
-export function apiGetNews() {
-  return request({
-    url: "/api/v1/home/news/",
-    method: "get",
-  });
-}
+export const apiGetNews = () => {
+  return request.get(`/api/v1/home/news/`);
+};
