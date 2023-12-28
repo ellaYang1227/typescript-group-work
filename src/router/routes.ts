22dde5db10affd641e46792833379decd63086f3
Yilee <yileefrontend@gmail.com>
2023-12-28 23:43:49 +0800

    feat: 新增 登入功能

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,86 +1,92 @@
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('@/views/Home.vue'),
+    path: "/",
+    name: "Home",
+    component: () => import("@/views/Home.vue"),
     meta: {
-      title: '首頁', //添加這部分
+      title: "首頁", //添加這部分
     },
   },
   {
-    path: '/rooms',
-    name: 'rooms',
-    component: () => import('@/views/Rooms/Index.vue'),
+    path: "/rooms",
+    name: "rooms",
+    component: () => import("@/views/Rooms/Index.vue"),
     meta: {
-      title: '客房資訊',
+      title: "客房資訊",
     },
   },
   {
-    path: '/rooms/:id',
-    name: 'rooms-item',
-    component: () => import('@/views/Rooms/Item.vue'),
+    path: "/rooms/:id",
+    name: "rooms-item",
+    component: () => import("@/views/Rooms/Item.vue"),
     meta: {
-      title: '客房資訊',
+      title: "客房資訊",
     },
   },
   {
-    path: '/rooms/:id/reservation',
-    name: 'rooms-reservation',
-    component: () => import('@/views/Rooms/Reservation.vue'),
+    path: "/rooms/:id/reservation",
+    name: "rooms-reservation",
+    component: () => import("@/views/Rooms/Reservation.vue"),
     meta: {
-      title: '客房預約',
+      title: "客房預約",
       requiresAuth: true,
     },
   },
   {
-    path: '/rooms/:id/success',
-    name: 'rooms-success',
-    component: () => import('@/views/Rooms/ReservationSuccess.vue'),
+    path: "/rooms/:id/success",
+    name: "rooms-success",
+    component: () => import("@/views/Rooms/ReservationSuccess.vue"),
     meta: {
-      title: '客房預約成功',
+      title: "客房預約成功",
       requiresAuth: true,
     },
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/Login.vue'),
-    meta: {
-      title: '會員登入',
-    },
+    path: "/",
+    component: () => import("@/components/Layouts/Login.vue"),
+    children: [
+      {
+        path: "login",
+        name: "login",
+        component: () => import("@/views/Login.vue"),
+        meta: {
+          title: "會員登入",
+        },
+      },
+    ],
   },
   {
-    path: '/user',
-    name: 'user',
-    component: () => import('@/views/User/Index.vue'),
+    path: "/user",
+    name: "user",
+    component: () => import("@/views/User/Index.vue"),
     meta: {
-      title: '會員資訊',
+      title: "會員資訊",
       requiresAuth: true,
     },
   },
   {
-    path: '/user/orders',
-    name: '/user/orders',
-    component: () => import('@/views/User/Orders.vue'),
+    path: "/user/orders",
+    name: "/user/orders",
+    component: () => import("@/views/User/Orders.vue"),
     meta: {
-      title: '會員訂單',
+      title: "會員訂單",
       requiresAuth: true,
     },
   },
   {
-    path: '/guideline',
-    name: 'guideline',
-    component: () => import('@/views/Guideline.vue'),
+    path: "/guideline",
+    name: "guideline",
+    component: () => import("@/views/Guideline.vue"),
     meta: {
-      title: 'Guildline',
+      title: "Guildline",
     },
   },
   {
-    path: '/:pathMath(.*)*',
-    name: '404',
-    component: import('@/views/NotFound.vue'),
+    path: "/:pathMath(.*)*",
+    name: "404",
+    component: import("@/views/NotFound.vue"),
     meta: {
-      title: '404',
+      title: "404",
     },
   },
 ];
