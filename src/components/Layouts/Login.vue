b89bc1a3669a64d5d2ee3e42cc724aaf310383dc
Yilee <yileefrontend@gmail.com>
2023-12-31 21:08:52 +0800

    fix: 調整 login layout 畫面

--- src/components/Layouts/Login.vue
+++ src/components/Layouts/Login.vue
@@ -7,9 +7,8 @@ import { RouterView } from "vue-router";
   <Layout isHiddenFooter type="styleDisabled">
     <section class="row m-0 login-layout">
       <div class="col-md-6 image-bg d-none d-sm-block"></div>
-      <div
-        class="col-md-6 d-flex justify-content-center align-items-center p-3"
-      >
+      <div class="col-md-6 d-flex justify-content-center p-3 form-content">
+        <img class="deco-img" src="@/assets/loginDeco.svg" alt="deco" />
         <RouterView class="form-area w-100" />
       </div>
     </section>
@@ -21,13 +20,41 @@ import { RouterView } from "vue-router";
   background-color: $neutral-dark;
   color: white;
   min-height: calc(100vh - 120px); //減去header
+  position: relative;
+  overflow: hidden;
   .image-bg {
     background-image: url("https://images.unsplash.com/photo-1630587148265-761cbd139043?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     background-size: cover;
     background-position: center;
+    z-index: 1;
   }
-  .form-area {
-    max-width: 416px;
+  .form-content {
+    align-items: center;
+    .form-area {
+      max-width: 416px;
+      z-index: 1;
+    }
+    .deco-img {
+      position: absolute;
+      top: 72px;
+      right: 0;
+    }
+  }
+  :deep(.form-control.is-invalid) {
+    background-image: none;
+  }
+  @media (max-width: 768px) {
+    min-height: calc(100vh - 72px);
+    .form-content {
+      align-items: normal;
+      .form-area {
+        max-width: 335px;
+      }
+      .deco-img {
+        top: 32px;
+        transform: scale(1.2);
+      }
+    }
   }
 }
 </style>
