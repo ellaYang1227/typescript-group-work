b30440b14664dfc8d8d424c20ec32692d5881e29
ssuyun.chen <ssuyun.chen@udngroup.com>
2024-01-08 11:48:27 +0800

    調整歷史訂單列表、即將來的行程顯示

--- src/views/User/Orders.vue
+++ src/views/User/Orders.vue
@@ -1,42 +1,69 @@
 <script setup lang="ts">
 import Layout from "@/components/Layouts/Index.vue";
-// import ProvideItemsCard from "@/components/Room/ProvideItemsCard.vue";
 import OrderDetailCard from "@/components/Order/OrderDetailCard.vue";
 import { swalWithCheckButtons } from "@/utilities/sweetAlert";
 import { OrderDetail } from "@/interfaces/orderDetail";
 import { getOrders, deleteOneOrder } from "@/models/orders";
-import { ref, watch } from "vue";
+import { ref, computed } from "vue";
 
 const showAll = ref(false);
 const orderDetails = ref<OrderDetail[]>([]);
-const lastOrderDetail = ref<null | OrderDetail>(null);
-watch(orderDetails, () => {
-  if (orderDetails.value?.length) {
-    lastOrderDetail.value = orderDetails.value[orderDetails.value.length - 1];
-  }
-});
+const featureOrderDetail = ref<null | OrderDetail>(null);
+
+// 歷史訂單只顯示有預約的訂單
+const filterOrders = computed(() =>
+  orderDetails.value.filter((item) => item.status === 0)
+);
+
+// 即將來的行程顯示入住時間最靠近今天的日期
+function getFeatureOrder() {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // 將今天的時間設為 00:00:00
+  featureOrderDetail.value = null;
+  // 1) 過濾掉昨天以前的訂單
+  const futureOrders = filterOrders.value.filter((order) => {
+    const orderDate = new Date(order.checkInDate);
+    return orderDate >= today;
+  });
+  // 2) 如果沒有即將來的行程，則不顯示
+  if (futureOrders.length === 0) return [];
+  // 3) 依照入住時間排序
+  const sortedOrders = [...futureOrders].sort(
+    (a, b) =>
+      (new Date(a.checkInDate) as any) - (new Date(b.checkInDate) as any)
+  );
+  // 4) 取得最靠近今天的訂單
+  featureOrderDetail.value = sortedOrders.reduce((prev, current) => {
+    const prevDate = new Date(prev.checkInDate);
+    const currentDate = new Date(current.checkInDate);
+    return prevDate >= today && prevDate <= currentDate ? prev : current;
+  });
+}
 
 // 取消單筆訂單
 function cancelOrder() {
-  swalWithCheckButtons.fire({
-    icon: "info",
-    title: "取消預定",
-    text: "確定要取消此房型的預訂嗎?",
-    showCancelButton: true,
-    showConfirmButton: true,
-    cancelButtonText: "關閉視窗",
-    confirmButtonText: "確定取消",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      const id = lastOrderDetail.value!._id;
-      deleteOneOrder(id);
-    }
-  })
-};
+  swalWithCheckButtons
+    .fire({
+      icon: "info",
+      title: "取消預定",
+      text: "確定要取消此房型的預訂嗎?",
+      showCancelButton: true,
+      showConfirmButton: true,
+      cancelButtonText: "關閉視窗",
+      confirmButtonText: "確定取消",
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        const id = featureOrderDetail.value!._id;
+        deleteOneOrder(id).then(() => handleGetOrders());
+      }
+    });
+}
 
-// 撈取訂單資料
+// 撈取所有訂單資料，並取得即將來的行程
 async function handleGetOrders() {
   orderDetails.value = await getOrders();
+  getFeatureOrder();
 }
 handleGetOrders();
 </script>
@@ -45,11 +72,21 @@ handleGetOrders();
   <Layout is-has-footer-decoration>
     <section class="bg-neutral-dark user">
       <div class="banner">
-        <img class="banner__bg w-100 d-none d-lg-block" src="@/assets/userBanner.png" alt="userBanner">
-        <img class="banner__bg w-100 d-block d-lg-none" src="@/assets/userBannerMobile.jpg" alt="userBanner">
+        <img
+          class="banner__bg w-100 d-none d-lg-block"
+          src="@/assets/userBanner.png"
+          alt="userBanner"
+        />
+        <img
+          class="banner__bg w-100 d-block d-lg-none"
+          src="@/assets/userBannerMobile.jpg"
+          alt="userBanner"
+        />
         <div class="banner__container container">
-          <div class="banner__head d-flex flex-column flex-lg-row align-items-lg-center gap-3 gap-lg-4">
-            <img src="@/assets/headSticker.png" alt="headSticker">
+          <div
+            class="banner__head d-flex flex-column flex-lg-row align-items-lg-center gap-3 gap-lg-4"
+          >
+            <img src="@/assets/headSticker.png" alt="headSticker" />
             <p class="text-white">Hello，Jessica</p>
           </div>
         </div>
@@ -71,12 +108,23 @@ handleGetOrders();
             我的訂單
           </router-link>
         </nav>
-        <main class="row gap-4 gap-lg-6 d-flex flex-column flex-lg-row w-100 m-auto">
-          <div class="card col-12 col-lg-6 col-xxl-6 p-0 h-100" v-if="lastOrderDetail">
-            <OrderDetailCard :orderDetails="[lastOrderDetail]">
+        <main
+          class="row gap-4 gap-lg-6 d-flex flex-column flex-lg-row w-100 m-auto"
+        >
+          <div
+            class="card col-12 col-lg-6 col-xxl-6 p-0 h-100"
+            v-if="featureOrderDetail"
+          >
+            <OrderDetailCard :orderDetails="[featureOrderDetail]">
               <div class="d-flex justify-content-between gap-3 pt-6">
-                <button class="baseButton isStyleSecondary w-100" @click="cancelOrder">取消預定</button>
-                <RouterLink :to="`/rooms/${lastOrderDetail.roomId._id}`" 
+                <button
+                  class="baseButton isStyleSecondary w-100"
+                  @click="cancelOrder"
+                >
+                  取消預定
+                </button>
+                <RouterLink
+                  :to="`/rooms/${featureOrderDetail.roomId._id}`"
                   class="baseButton isStylePrimary w-100"
                 >
                   查看詳情
@@ -84,16 +132,19 @@ handleGetOrders();
               </div>
             </OrderDetailCard>
           </div>
-          <div class="card col-12 col-lg p-0" v-if="orderDetails.length > 0">
+          <div
+            class="card col-12 col-lg p-0 h-100"
+            v-if="filterOrders.length > 0"
+          >
             <OrderDetailCard
               :isHistory="true"
-              :orderDetails="orderDetails"
+              :orderDetails="filterOrders"
               :isShowAll="showAll"
             >
               <button
                 class="baseButton isStyleSecondary w-100"
                 @click="showAll = !showAll"
-                v-if="orderDetails.length > 3 && !showAll"
+                v-if="filterOrders.length > 3 && !showAll"
               >
                 查看更多<font-awesome-icon class="ps-3" icon="chevron-down" />
               </button>
